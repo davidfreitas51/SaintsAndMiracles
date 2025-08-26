6e981cea774830df93692e2b48a3f487284143b5
David Freitas <davidfreitas51@proton.me>
2025-08-25 22:42:59 -0300

    feat: Admin header style changes and menu dropdown

--- Client/src/app/shared/components/admin-header/admin-header.component.ts
+++ Client/src/app/shared/components/admin-header/admin-header.component.ts
@@ -1,26 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AccountsService } from '../../../core/services/accounts.service';
 import { Router, RouterLink } from '@angular/router';
+import { SnackbarService } from '../../../core/services/snackbar.service';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-header',
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink, MatIconModule, CommonModule],
   templateUrl: './admin-header.component.html',
   styleUrl: './admin-header.component.scss',
 })
-export class AdminHeaderComponent {
+export class AdminHeaderComponent implements OnInit {
   private accountService = inject(AccountsService);
+  private snackBarService = inject(SnackbarService);
   private router = inject(Router);
+
+  userName = '';
+  menuOpen = false;
+
+  ngOnInit(): void {
+    this.accountService.currentUser$.subscribe({
+      next: (user) => {
+        if (user) {
+          this.userName = `${user.firstName} ${user.lastName}`;
+        }
+      },
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 
   handleLogout() {
     this.accountService.logout().subscribe({
       next: () => {
-        this.router.navigate(['/']); 
+        this.router.navigate(['/']);
+        this.snackBarService.success("You've logged out");
       },
       error: (err) => {
-        console.error('Logout failed', err);
+        this.snackBarService.error('Logout failed');
       },
     });
   }
