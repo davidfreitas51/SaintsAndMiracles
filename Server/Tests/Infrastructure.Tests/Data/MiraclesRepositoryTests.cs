06d984af1c56289ae24f5e47258abd9fea9f96e5
David Freitas <davidfreitas51@proton.me>
2025-11-08 15:44:58 -0300

    test: Testing basic test setup

--- Server/Tests/Infrastructure.Tests/Data/MiraclesRepositoryTests.cs
+++ Server/Tests/Infrastructure.Tests/Data/MiraclesRepositoryTests.cs
@@ -1,6 +1,18 @@
-namespace Infrastructure.Tests.Data;
+using Core.Models;
 
 public class MiraclesRepositoryTests
 {
 
+    [Fact]
+    public async Task GetByIdAsync_ReturnsMiracle_WhenExists()
+    {
+        var a = true;
+        Assert.True(a);
+    }
+
+    [Fact]
+    public async Task SlugExistsAsync_ReturnsTrue_WhenSlugExists()
+    {
+
+    }
 }
